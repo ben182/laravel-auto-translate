32b48b31d2bb56e5962bd8183beb0985f57abb49
Benjamin Bortels <moin@benjaminbortels.de>
2019-02-13 13:50:20 +0100

    tests & travis conf

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -2,16 +2,56 @@
 
 namespace Ben182\AutoTranslate\Tests;
 
+use Ben182\AutoTranslate\Commands\AllCommand;
+use Ben182\AutoTranslate\Commands\MissingCommand;
+
+
 class CommandTest extends TestCase
 {
-    // public function test_AllCommand() {
+    public function test_AllCommand() {
 
-    //     $this->createTempFiles([
-    //         'en' => [
-    //             'user' => "<?php\n return ['name' => 'Name', 'age' => 'Age'];",
-    //         ],
-    //     ]);
+        $this->createTempFiles([
+            'en' => [
+                'user' => "<?php\n return ['name' => 'Name', 'age' => 'Age'];",
+            ],
+        ]);
 
-    //     $this->artisan('autotrans:all');
-    // }
+        try {
+            (new AllCommand(app('auto-translate')))->handle();
+        } catch (\Throwable $th) {}
+
+        $translations =app('auto-translate')->getTranslations('de');
+
+        $this->assertEquals([
+            'user' => [
+                'name' => 'Name',
+                'age' => 'Alter',
+            ],
+        ], $translations);
+    }
+
+    public function test_MissingCommand() {
+
+        $this->createTempFiles([
+            'en' => [
+                'user' => "<?php\n return ['name' => 'Name', 'age' => 'Age'];",
+            ],
+            'de' => [
+                'user' => "<?php\n return ['name' => 'Name'];",
+            ],
+        ]);
+
+        try {
+            (new MissingCommand(app('auto-translate')))->handle();
+        } catch (\Throwable $th) {}
+
+        $translations = app('auto-translate')->getTranslations('de');
+
+        $this->assertEquals([
+            'user' => [
+                'name' => 'Name',
+                'age' => 'Alter',
+            ],
+        ], $translations);
+    }
 }
