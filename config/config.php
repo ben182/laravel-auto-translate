8932623048aabddf9566e781c7db99254007b8f9
Ben <moin@benjaminbortels.de>
2019-02-13 20:30:40 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/config.php
+++ config/config.php
@@ -1,22 +1,22 @@
 <?php
 
 return [
-    /**
+    /*
      * Here you can specify the source language code.
      */
     'source_language' => 'en',
 
-    /**
+    /*
      * Here you can specify the target language code(s). This can be a string or an array.
      */
     'target_language' => ['de'],
 
-    /**
+    /*
      * Specify the path to the translation files.
      */
     'path' => realpath(base_path('resources/lang')),
 
-    /**
+    /*
      * This is the translator used to translate the source language files. You can also specify your own here if you wish. It has to implement \Ben182\AutoTranslate\Translators\TranslatorInterface.
      */
     'translator' => \Ben182\AutoTranslate\Translators\SimpleGoogleTranslator::class,
