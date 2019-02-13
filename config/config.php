8b33b7b9eea6bd86e0df6df2833fbaf869087e2a
Benjamin Bortels <moin@benjaminbortels.de>
2019-02-13 21:30:53 +0100

    Clean Up

--- config/config.php
+++ config/config.php
@@ -1,11 +1,23 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
+    /**
+     * Here you can specify the source language code.
+     */
     'source_language' => 'en',
+
+    /**
+     * Here you can specify the target language code(s). This can be a string or an array.
+     */
     'target_language' => ['de'],
-    'translator' => \Ben182\AutoTranslate\Translators\SimpleGoogleTranslator::class,
+
+    /**
+     * Specify the path to the translation files.
+     */
     'path' => realpath(base_path('resources/lang')),
+
+    /**
+     * This is the translator used to translate the source language files. You can also specify your own here if you wish. It has to implement \Ben182\AutoTranslate\Translators\TranslatorInterface.
+     */
+    'translator' => \Ben182\AutoTranslate\Translators\SimpleGoogleTranslator::class,
 ];
--- src/AutoTranslateServiceProvider.php
+++ src/AutoTranslateServiceProvider.php
@@ -15,33 +15,10 @@ class AutoTranslateServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-auto-translate');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-auto-translate');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('auto-translate.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-auto-translate'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-auto-translate'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-auto-translate'),
-            ], 'lang');*/
 
             // Registering package commands.
             $this->commands([
